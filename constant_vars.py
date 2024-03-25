ea0c2e29bf500662a781b9c0f256dc0e7064777b
lance-go <lance.dinh.goldenowl@gmail.com>
2024-03-25 17:26:39 +0700

    Add file

---
+++ constant_vars.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials, firestore
+import stripe
+import os
+
+load_dotenv()
+
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred)
+
+db=firestore.client()
+
+stripe_keys = {
+    "secret_key": os.getenv("SECRET_KEY"),
+    "publishable_key": os.getenv("PUBLISHABLE_KEY"),
+}
+
+endpoint_secret = os.getenv("ENDPOINT_SECRET")
---
+++ flask_server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import stripe
+from constant_vars import endpoint_secret, db, stripe_keys
+
+app = Flask(__name__)
+
+stripe.api_key = stripe_keys["secret_key"]
+
+@app.route("/stripe-checkout", methods=["POST"])
+def stripe_webhook():
+    payload = request.get_data(as_text=True)
+    id = request.args.get('id')
+    print(id)
+    sig_header = request.headers.get("Stripe-Signature")
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+
+    except ValueError as e:
+        # Invalid payload
+        return "Invalid payload", 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return "Invalid signature", 400
+
+    # Handle the payment_intent.succeeded event
+    if event["type"] == "payment_intent.succeeded":
+        print("Payment was successful.")
+        # TODO: run some custom code here
+
+    return "Success", 200
+
+# main driver function
+if __name__ == '__main__':
+	app.run()
